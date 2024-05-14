d332378ddc29ee897de995970594f236a78995bb
wzc345 <152698688+wzc345@users.noreply.github.com>
2024-05-14 21:45:51 +0800

    Add files via upload

---
+++ 校运会.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<string,string> m;
+string Find(string name){
+	if(m[name]!=name){
+		m[name]=Find(m[name]);
+	}
+	return m[name];
+}
+void Union(string n1,string n2){
+	string root1=Find(n1);
+	string root2=Find(n2);
+	if(root1!=root2){
+		m[root1]=root2;
+	}
+}
+int main(){
+	int N,M;
+	cin>>N>>M;
+	while(N--){
+		string name;
+		cin>>name;
+		m[name]=name;
+	}
+	while(M--){
+		string n1,n2;
+		cin>>n1>>n2;
+		Union(n1,n2);
+	}
+	int K;
+	cin>>K;
+	while(K--){
+		string f1,f2;
+		cin>>f1>>f2;
+		if(Find(f1)!=Find(f2)) cout<<"No."<<endl;
+		else cout<<"Yes."<<endl;
+	}
+}
