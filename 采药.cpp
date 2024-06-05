2bf1ed97b219fffe67387dfb5b5069aa9a81072a
wzc345 <152698688+wzc345@users.noreply.github.com>
2024-06-05 12:44:15 +0800

    Update 采药.cpp

--- 采药.cpp
+++ 采药.cpp
@@ -18,3 +18,21 @@ int main(){
 	}
 	cout<<dp[M][T];
 }
+//状态压缩成一维
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	int T,M;
+//	cin>>T>>M;
+//	vector<int> time(M+1,0),value(M+1,0);
+//	vector<int> dp(T+1,0);
+//	for(int i=1;i<=M;i++){
+//		cin>>time[i]>>value[i];
+//	} 
+//	for(int i=1;i<=M;i++){
+//		for(int j=T;j>=0;j--){
+//			if(j>=time[i]) dp[j]=max(dp[j-time[i]]+value[i],dp[j]);
+//		}
+//	}
+//	cout<<dp[T];
+//}
